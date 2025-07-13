b1d80b914f2b9b1c9c12c218e2223cdf363ab06c
prakritish <bhattacharyaprakritish@gmail.com>
2025-07-14 03:40:55 +0530

    Auto commit on 14/7/2025, 3:40:54 am

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,17 @@
+import { LOGO_URL } from "../utils/constants"
 const Header = ()=>{
     return(
         <div className="mother-container">
             <div className="logo-image">
-                <img className="logo" src="{LOGO_URL}"></img>
+                <img className="logo" src={LOGO_URL}></img>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>Contacts</li>
+                    <li>Menu</li>
+                    <li>Cart</li>
+                </ul>
             </div>
         </div>
     )
