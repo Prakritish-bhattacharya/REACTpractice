07587dbe570bc2692f58490e49b967fcc17fe5c1
prakritish <bhattacharyaprakritish@gmail.com>
2025-07-14 03:08:40 +0530

    Auto commit on 14/7/2025, 3:08:40 am

---
+++ src/Components/Header.js
@@ -0,0 +1,10 @@
+const Header = ()=>{
+    return(
+        <div className="mother-container">
+            <div className="logo-image">
+                <img className="logo" src="{LOGO_URL}"></img>
+            </div>
+        </div>
+    )
+}
+export default Header
---
+++ src/utils/constants.js
@@ -0,0 +1,1 @@
+export const LOGO_URL = "../assets/medicine-shop-final-logo"
