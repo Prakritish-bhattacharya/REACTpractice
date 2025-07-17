df17f93a0849cb9622e83e785f8da0c12e741939
prakritish <bhattacharyaprakritish@gmail.com>
2025-07-18 00:35:42 +0530

    Auto commit on 18/7/2025, 12:35:42 am

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react"
 import ReactDOM  from "react-dom/client"
 import Header from "./Components/Header"
+import Body from "./Components/Body"
+
 // const AppLayout = ()=>{
 //     return(
 //         <div className="app">
@@ -15,6 +17,7 @@ const AppLayout =()=>{
         
         <div className="app">
             <Header/>
+            <Body/>
         </div>
     )
 }
---
+++ src/Components/RestaurantCard.js
@@ -0,0 +1,8 @@
+const RestaurantCard = ()=>{
+    return(
+        <div className="res-card">
+            <h3>Meghana Foods</h3>
+        </div>
+    )
+}
+export default RestaurantCard
